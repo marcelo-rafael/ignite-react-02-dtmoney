53109ec1c35c4302297034c4474c54204a23d565
Marcelo Rafael Gonçalves <marcelo.rafael.goncalves@gmail.com>
2022-02-20 21:27:59 -0300

    Configurando MirageJS

--- src/components/TransactionsTable/index.tsx
+++ src/components/TransactionsTable/index.tsx
@@ -1,6 +1,14 @@
+import { useEffect } from 'react'
+
 import * as S from './styles'
 
 export function TransactionsTable() {
+  useEffect(() => {
+    fetch('ttp://localhost:3000/api/transactions')
+      .then(response => response.json())
+      .then(data => console.log(data))
+  }, [])
+
   return (
     <S.Container>
       <table>
